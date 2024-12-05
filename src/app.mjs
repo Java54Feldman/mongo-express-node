35c69a88367aa8d12c35744d5c63a821e6770f40
Pavel Feldman <feldmanpk@gmail.com>
2024-12-05 21:47:54 +0200

    CW #74 introducing Basic authentication

--- src/app.mjs
+++ src/app.mjs
@@ -4,8 +4,12 @@ import { errorHandler } from "./errors/error.mjs";
 import schemas from "./validation-schemas/schemas.mjs";
 import { mflix_route } from "./routes/mflix.mjs";
 import { accounts_route } from "./routes/accounts.mjs";
+import AccountsService from "./service/AccountsService.mjs";
+import { ADD_UPDATE_ACCOUNT } from "./config/pathes.mjs";
+import { auth, authenticate } from "./middleware/authentication.mjs";
 const app = express();
 const port = process.env.PORT || 3500;
+export const accountsService = new AccountsService(process.env.MONGO_URI, process.env.DB_NAME);
 
 const server = app.listen(port);
 server.on("listening", () =>
@@ -13,6 +17,8 @@ server.on("listening", () =>
 );
 
 app.use(express.json());
+app.use(authenticate(accountsService));
+app.use(auth(JSON.stringify({path:ADD_UPDATE_ACCOUNT, method:"POST"})))
 app.use(validateBody(schemas));
 app.use(valid);
 app.use("/mflix", mflix_route);
